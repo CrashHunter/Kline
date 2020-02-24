b103fb3d3c4e173d1a023f102c0e6610ca8136eb
CrashHunter <eclowang@gmail.com>
2020-02-24 13:21:52 +0800

    update : update VolumeActivity

--- app/src/main/java/org/crashhunter/kline/VolumeActivity.kt
+++ app/src/main/java/org/crashhunter/kline/VolumeActivity.kt
@@ -11,6 +11,8 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class VolumeActivity : AppCompatActivity() {
@@ -27,15 +29,21 @@ class VolumeActivity : AppCompatActivity() {
 //        OKHTTPRequest()
 
 
+        getFromRetrofit("USDT")
+
+
+    }
+
+    private fun getFromRetrofit(coin: String) {
         val retrofit = Retrofit.Builder()
             .baseUrl("https://min-api.cryptocompare.com/data/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
 
-        val service = retrofit.create(GitHubService::class.java)
+        val service = retrofit.create(KlineService::class.java)
 
 
-        val call: Call<CoinVolume?>? = service.listRepos("BTC")
+        val call: Call<CoinVolume?>? = service.queryVolume(coin)
 
 
         call!!.enqueue(object : Callback<CoinVolume?> {
@@ -43,14 +51,30 @@ class VolumeActivity : AppCompatActivity() {
             override fun onResponse(call: Call<CoinVolume?>, response: Response<CoinVolume?>) {
 
                 var str = StringBuilder()
-                str.append("BTC: \n")
-//                str.append(response.raw().body?.string() + " \n")
+                //                str.append(response.raw().body?.string() + " \n")
+
+                var preVolumeStr = ""
+
                 for (data in response.body()?.data!!) {
+                    var rateStr = ""
+                    var volumeStr = data.topTierVolumeTotal
 
-                    var volume = data.topTierVolumeTotal.toString()
+                    if (!preVolumeStr.isEmpty()) {
 
-                    str.append("  $volume \n")
+                        var preValue = preVolumeStr.toBigDecimal()
+                        var currentValue = volumeStr.toBigDecimal()
 
+                        var rate = currentValue / preValue
+                        rateStr = "-- " + rate.toString() + "%"
+
+                    }
+                    preVolumeStr = volumeStr
+
+                    val date = Date(data.time.toLong() * 1000)
+                    val format = SimpleDateFormat("yyyy.MM.dd")
+                    var day = format.format(date)
+
+                    str.append("${day} : $volumeStr ${rateStr}\n")
 
                 }
 
@@ -64,8 +88,6 @@ class VolumeActivity : AppCompatActivity() {
 
             }
         })
-
-
     }
 
     private fun OKHTTPRequest() {
@@ -109,12 +131,23 @@ class VolumeActivity : AppCompatActivity() {
 
     private fun initAction() {
 
+        request.setOnClickListener {
+
+            var coin = etCoin.text.toString()
+
+            if (coin.isNotEmpty()) {
+
+            }
+
+
+        }
+
     }
 
 
-    interface GitHubService {
+    interface KlineService {
         //https://min-api.cryptocompare.com/data/symbol/histoday?fsym=BTC&tsym=USD&limit=10&api_key=4789529a8c5e2a2e26d4c665fa74c50d497c8971a5f1a6785d2a556da615d57d
-        @GET("symbol/histoday?tsym=USD&limit=10&api_key=4789529a8c5e2a2e26d4c665fa74c50d497c8971a5f1a6785d2a556da615d57d")
-        fun listRepos(@Query("fsym") fsymCoin: String): Call<CoinVolume?>?
+        @GET("symbol/histoday?tsym=USD&limit=20&api_key=4789529a8c5e2a2e26d4c665fa74c50d497c8971a5f1a6785d2a556da615d57d")
+        fun queryVolume(@Query("fsym") fsymCoin: String): Call<CoinVolume?>?
     }
 }
